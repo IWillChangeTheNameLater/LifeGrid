86fe3b08743a4509f810823c8c69567a85903992
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-08-27 23:54:23 +0500

    Set up the JWT configuration

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,15 +4,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env')
 
-    db_host: str = "localhost"
+    db_host: str = 'localhost'
     db_port: int = 5432
-    db_user: str = "postgres"
-    db_pass: str = "postgres"
-    db_name: str = "postgres"
+    db_user: str = 'postgres'
+    db_pass: str = 'postgres'
+    db_name: str = 'postgres'
 
     @property
     def db_dsn(self) -> str:
         return f'postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}'
+
+    refresh_jwt_key: str = 'Secret key for the refresh token'
+    access_jwt_key: str = 'Secret key for the access token'
+    jwt_algorithm: str = 'HS256'
+    access_token_exp: int = 60*15
+    refresh_token_exp: int = 60*60*24*30
 
 
 settings = Settings()
