d25cab702814dfbef0f29317986d54e174c730a7
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-08-29 19:24:19 +0500

    Remove Users' "nickname" attribute as unnecessary

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -4,6 +4,5 @@ from sqlmodel import Field, SQLModel
 
 class Users(SQLModel, table=True):
     id: int|None = Field(default=None, primary_key=True)
-    nickname: str
     email: EmailStr
     hashed_password: str
