3e05c9313fb040f23d09741c628b8ea5cb4a2791
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-10-24 21:23:27 +0500

    Add the base model for users

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -2,7 +2,11 @@ from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
-class Users(SQLModel, table=True):
+class BaseUsers(SQLModel):
+    ...
+
+
+class Users(BaseUsers, table=True):
     id: int|None = Field(default=None, primary_key=True)
     email: EmailStr
     hashed_password: str
