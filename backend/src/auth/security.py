60f415e5f386e07bd52755f43b38379a484253d3
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-10-26 00:47:04 +0500

    Fix the wrong import

--- backend/src/auth/security.py
+++ backend/src/auth/security.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, UTC
 import bcrypt, jwt
 from pydantic import EmailStr
 
-from config import Settings
+from config import settings
 from users.dao import UsersDAO
 from users.models import Users
 
@@ -19,33 +19,33 @@ def verify_password(password: str, hashed_password: str) -> bool:
 def create_jwt(payload: dict, seconds_to_expire: int, key: str) -> str:
     payload = payload.copy()
     payload['exp'] = datetime.now(UTC) + timedelta(seconds=seconds_to_expire)
-    encoded_jwt = jwt.encode(payload, key, Settings.jwt_algorithm)
+    encoded_jwt = jwt.encode(payload, key, settings.jwt_algorithm)
     return encoded_jwt
 
 
 def create_access_jwt(payload: dict) -> str:
     return create_jwt(
-        payload, Settings.access_jwt_exp_sec, Settings.access_jwt_key
+        payload, settings.access_jwt_exp_sec, settings.access_jwt_key
     )
 
 
 def create_refresh_jwt(payload: dict) -> str:
     return create_jwt(
-        payload, Settings.refresh_jwt_exp_sec, Settings.refresh_jwt_key
+        payload, settings.refresh_jwt_exp_sec, settings.refresh_jwt_key
     )
 
 
 def extract_jwt_payload(token: str, key: str) -> dict:
-    payload: dict = jwt.decode(token, key, algorithms=[Settings.jwt_algorithm])
+    payload: dict = jwt.decode(token, key, algorithms=[settings.jwt_algorithm])
     return payload
 
 
 def extract_access_jwt_payload(token: str) -> dict:
-    return extract_jwt_payload(token, Settings.access_jwt_key)
+    return extract_jwt_payload(token, settings.access_jwt_key)
 
 
 def extract_refresh_jwt_payload(token: str) -> dict:
-    return extract_jwt_payload(token, Settings.refresh_jwt_key)
+    return extract_jwt_payload(token, settings.refresh_jwt_key)
 
 
 async def authenticate_user(email: EmailStr, password: str) -> Users|None:
