269674431b9b948546cd601b7007d27b72cfefbd
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-11-03 21:21:26 +0500

    Extract the logic of creating tokens

--- backend/src/auth/utils.py
+++ backend/src/auth/utils.py
@@ -1,6 +1,16 @@
 from fastapi import Response
 
-from auth.models import Tokens
+from auth.models import AccessTokenPayload, RefreshTokenPayload, Tokens
+from auth.security import create_access_jwt, create_refresh_jwt
+from users.models import Users
+
+
+def create_tokens_from_user(user: Users) -> Tokens:
+    access_jwt = create_access_jwt(
+        AccessTokenPayload(sub=user.id, email=user.email)
+    )
+    refresh_jwt = create_refresh_jwt(RefreshTokenPayload(sub=user.id))
+    return Tokens(access_token=access_jwt, refresh_token=refresh_jwt)
 
 
 def set_tokens_in_cookies(response: Response, tokens: Tokens) -> None:
