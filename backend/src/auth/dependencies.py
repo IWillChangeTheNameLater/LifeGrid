2d63ce70e647b95c632df18c6be3db78062451d1
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-11-03 20:54:03 +0500

    Add a dependency to get the refresh token payload

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, Request, status
 import jwt
 
-from auth.models import AccessTokenPayload
+from auth.models import AccessTokenPayload, RefreshTokenPayload
 from auth.security import _extract_payload_from_jwt
 from config import settings
 
@@ -22,6 +22,20 @@ def get_access_jwt_payload(
     try:
         payload = _extract_payload_from_jwt(token, settings.access_jwt_key)
         return AccessTokenPayload(**payload)
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    except TypeError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+def get_refresh_jwt_payload(
+    token: str = Depends(_get_refresh_jwt)
+) -> RefreshTokenPayload:
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    try:
+        payload = _extract_payload_from_jwt(token, settings.refresh_jwt_key)
+        return RefreshTokenPayload(**payload)
     except jwt.PyJWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     except TypeError:
