a732d7e4c9b5367caa483c81fa579ab269627cd1
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-11-03 22:34:55 +0500

    Add various exceptions

---
+++ backend/src/exceptions.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException, status
+
+
+class LifeGridException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = ''
+
+    def __init__(self) -> None:
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(LifeGridException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = 'The user already exists'
+
+
+class IncorrectEmailOrPasswordException(LifeGridException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Incorrect email or password'
+
+
+class TokenExpiredException(LifeGridException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Token has expired'
+
+
+class TokenAbsentException(LifeGridException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Token is absent'
+
+
+class IncorrectTokenFormatException(LifeGridException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Incorrect token format'
+
+
+class UserIsNotPresentException(LifeGridException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'User is not present'
