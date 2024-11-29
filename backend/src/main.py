4044e8616c214c404b692660da0c4a3862aab998
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-11-29 23:56:09 +0500

    Run the app through the main file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 from auth.router import router as router_auth
 
 
 app = FastAPI()
 app.include_router(router_auth)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app')
