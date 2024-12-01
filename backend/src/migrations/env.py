7be0dacc676807f4fc768334b57cfdf428a0cb5c
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-01 12:55:31 +0500

    Minor changes

--- backend/src/migrations/env.py
+++ backend/src/migrations/env.py
@@ -7,15 +7,13 @@ sys.path.insert(0, str(parent_dir))
 
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-
-from config import settings
+from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
-from users.models import Users
+
 from auth.models import IssuedRefreshTokens
+from config import settings
+from users.models import Users
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
