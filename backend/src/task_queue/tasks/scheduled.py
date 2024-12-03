190ff022cdd16ea21e0b2acbf64e3e9d229730d8
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-03 22:34:33 +0500

    Set time limits for the tokens deleting operation

--- backend/src/task_queue/tasks/scheduled.py
+++ backend/src/task_queue/tasks/scheduled.py
@@ -7,6 +7,6 @@ from task_queue.worker import celery_app
 event_loop = asyncio.get_event_loop()
 
 
-@celery_app.task
+@celery_app.task(time_limit=30*60, soft_time_limit=20*60)
 def delete_expired_tokens() -> None:
     event_loop.run_until_complete(IssuedTokensDAO.delete_expired())
