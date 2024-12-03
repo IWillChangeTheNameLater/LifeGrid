1f5f7aa842f9a32cd8c0909a840e3a083cbb30ca
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-03 21:52:45 +0500

    Use the current event loop to avoid conflicts

--- backend/src/task_queue/tasks/scheduled.py
+++ backend/src/task_queue/tasks/scheduled.py
@@ -4,6 +4,9 @@ from auth.dao import IssuedTokensDAO
 from task_queue.worker import celery_app
 
 
+event_loop = asyncio.get_event_loop()
+
+
 @celery_app.task
 def delete_expired_tokens() -> None:
-    asyncio.run(IssuedTokensDAO.delete_expired())
+    event_loop.run_until_complete(IssuedTokensDAO.delete_expired())
