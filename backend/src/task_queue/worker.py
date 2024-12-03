71982637f4f5397d099401ee756f7af109f64e9e
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-04 00:44:21 +0500

    Change the Celery default queue name

--- backend/src/task_queue/worker.py
+++ backend/src/task_queue/worker.py
@@ -11,6 +11,8 @@ class CeleryQueue(Enum):
     LOW_PRIORITY = 'low_priority'
     HIGH_PRIORITY = 'high_priority'
 
+    DEFAULT = 'default'
+
 
 celery_app = Celery(
     main=Path(__file__).parent.name,
@@ -18,6 +20,9 @@ celery_app = Celery(
     backend=settings.redis_backend_dsn,
     include=['task_queue.scheduler', 'task_queue.tasks']
 )
+
+# Configuration
+celery_app.conf.task_default_queue = CeleryQueue.DEFAULT.value
 
 if __name__ == '__main__':
     celery_app.start()
