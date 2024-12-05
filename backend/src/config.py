8d5d46ad2667b65fe7e4a9a02dbba2681b304c90
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-05 15:18:52 +0500

    Adjust the Pydantic Settings validation

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,7 +1,7 @@
 from enum import IntEnum, unique
 from pathlib import Path
 
-from pydantic import EmailStr
+from pydantic import EmailStr, PositiveInt
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -18,7 +18,7 @@ class _Settings(BaseSettings):
     )
 
     db_host: str = 'localhost'
-    db_port: int = 5432
+    db_port: PositiveInt = 5432
     db_user: str = 'postgres'
     db_pass: str = 'postgres'
     db_name: str = 'postgres'
@@ -30,14 +30,14 @@ class _Settings(BaseSettings):
     refresh_token_key: str = 'Secret key for the refresh token'
     access_token_key: str = 'Secret key for the access token'
     token_crypt_algorithm: str = 'HS256'
-    access_token_exp_sec: int = 60*60
-    refresh_token_exp_sec: int = 60*60*24*30
+    access_token_exp_sec: PositiveInt = 60*60
+    refresh_token_exp_sec: PositiveInt = 60*60*24*30
 
     redis_host: str = 'localhost'
-    redis_port: int = 6379
+    redis_port: PositiveInt = 6379
 
     smtp_host: str = 'smtp.gmail.com'
-    smtp_port: int = 465
+    smtp_port: PositiveInt = 465
     smtp_user: EmailStr = 'user@example.com'
     smtp_pass: str = 'SMTP password'
 
