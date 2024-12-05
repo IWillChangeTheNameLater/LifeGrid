b10d61e6c11956596b56c04e74e84e2c1342eb53
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-05 14:50:57 +0500

    Adjust the Celery configuration

--- backend/src/task_queue/celery_config.py
+++ backend/src/task_queue/celery_config.py
@@ -22,4 +22,9 @@ result_backend = f'redis://{settings.redis_host}:{settings.redis_port}/{RedisDB.
 task_create_missing_queues = False
 task_default_queue = CeleryQueue.DEFAULT.value
 task_queues = (Queue(q.value) for q in CeleryQueue)
-worker_max_memory_per_child = 10*1024
+worker_max_memory_per_child = 10*1024  # 10 megabytes
+task_acks_late = True
+task_time_limit = 3600  # 1 hour
+broker_transport_options = {
+    'visibility_timeout': 86400  # 1 day
+}
