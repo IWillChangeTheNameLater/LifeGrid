22bfe7e829c6006bf7250f7450ca6caf47eb39c4
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-07 00:20:21 +0500

    Add a function to verify user's email

--- backend/src/users/dao.py
+++ backend/src/users/dao.py
@@ -3,6 +3,7 @@ from sqlmodel import select
 
 from base_dao import BaseDAO
 from database import init_session
+from exceptions import *
 
 from .models import Users
 
@@ -16,3 +17,14 @@ class UsersDAO(BaseDAO[Users]):
             statement = select(cls.model).where(cls.model.email == email)
             results = await session.exec(statement)
             return results.first()
+
+    @classmethod
+    async def verify_email(cls, user_id: str) -> None:
+        async with init_session() as session:
+            user = await session.get(cls.model, user_id)
+            if user:
+                user.is_email_verified = True
+                session.add(user)
+                await session.commit()
+            else:
+                raise UserIsNotPresentException
