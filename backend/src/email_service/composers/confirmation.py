26afcd213109990ee4472b6c730c4643527044b6
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-06 12:18:11 +0500

    Add a function to read templates

--- backend/src/email_service/composers/confirmation.py
+++ backend/src/email_service/composers/confirmation.py
@@ -1,16 +1,13 @@
 from email.message import EmailMessage
-from pathlib import Path
+
+from ..utils import read_template
 
 
 def confirmation_email(confirmation_link: str) -> EmailMessage:
     msg = EmailMessage()
     msg['Subject'] = 'Confirm your email'
 
-    template_path = Path(
-        __file__
-    ).parent.parent/'templates'/'confirmation_email.html'
-    with open(template_path, 'r') as file:
-        body = file.read()
+    body = read_template('confirmation_email.html')
     body = body.replace('{confirmation_link}', confirmation_link)
     msg.set_content(body, subtype='html')
 
--- backend/src/email_service/utils.py
+++ backend/src/email_service/utils.py
@@ -1,5 +1,7 @@
 from email.message import EmailMessage
+from pathlib import Path
 from smtplib import SMTP_SSL
+import sys
 
 from pydantic import ConfigDict, EmailStr, validate_call
 
@@ -24,3 +26,18 @@ def send_email(
     with SMTP_SSL(settings.smtp_host, settings.smtp_port) as server:
         server.login(settings.smtp_user, settings.smtp_pass)
         server.send_message(msg, from_, to_)
+
+
+def read_template(
+    template_name: str|Path, templates_dir_path: str|Path|None = None
+) -> str:
+    template_path = Path(template_name)
+    if not template_path.is_absolute():
+        if templates_dir_path is None:
+            templates_dir_path = Path(sys.path[0])/'email_service'/'templates'
+        else:
+            templates_dir_path = Path(templates_dir_path)
+
+        template_path = templates_dir_path/template_name
+
+    return template_path.read_text()
