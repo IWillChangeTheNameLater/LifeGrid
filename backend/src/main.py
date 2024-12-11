cd449fb767df636ca48c06fe3c6d3b0b6e449f52
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-11 18:26:14 +0500

    Use a `noqa` comment instead of useless assignment

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 
-from common.task_queue import celery_app
 from domains.auth.router import router as router_auth
 
-# To ensure that the import will not be removed due to uselessness
-celery_app = celery_app
+from common.task_queue import celery_app  # noqa
+
 
 app = FastAPI()
 app.include_router(router_auth)
