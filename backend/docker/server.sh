d2d0c007f8eecf65145f8809081a72b58553f571
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-12-13 21:19:59 +0500

    Fix bug

--- backend/docker/server.sh
+++ backend/docker/server.sh
@@ -4,4 +4,4 @@ alembic revision --autogenerate
 alembic upgrade head
 
 cd src
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
